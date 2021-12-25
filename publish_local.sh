6fa14c5e81ee2575891ecb7db0fe8f1f5d6072ad
Jonah Bonner <jonahwbonner@gmail.com>
2021-12-25 14:59:02 -0500

    Publish wpilibio pom in "publish_local.sh"

--- publish_local.sh
+++ publish_local.sh
@@ -1,6 +1,6 @@
 # Publishes all artifacts to maven local to enable rapid testing with a GradleRIO FRC project
 
 bazel run //conduit/api:api-export.publish
-bazel run //conduit/wpilibio:nativezip.publish -c opt
+bazel run --define "publishing_nativezip_generate_pom=true" //conduit/wpilibio:nativezip.publish -c opt
 bazel run //junction/core:core-export.publish
 bazel run //junction/shims/wpilib:wpilib-export.publish
