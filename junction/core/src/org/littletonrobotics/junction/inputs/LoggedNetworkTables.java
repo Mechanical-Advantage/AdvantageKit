25bb60e5dc28106d0c67b4df30847e026be6d691
Jordan McMichael <jlmcmchl@gmail.com>
2022-11-02 15:42:21 -0400

    Disable NT Logging temporarily

--- junction/core/src/org/littletonrobotics/junction/inputs/LoggedNetworkTables.java
+++ junction/core/src/org/littletonrobotics/junction/inputs/LoggedNetworkTables.java
@@ -42,76 +42,76 @@ public class LoggedNetworkTables implements LoggableInputs {
   }
 
   public void toLog(LogTable table) {
-    for (int tableId = 0; tableId < tables.size(); tableId++) {
-      NetworkTableEntry[] entries = networkTables.getEntries(tables.get(tableId), 0);
-      for (int entryId = 0; entryId < entries.length; entryId++) {
-        NetworkTableEntry entry = entries[entryId];
-        String key = entry.getName().substring(1);
-        switch (entry.getType()) {
-          case kBoolean:
-            table.put(key, entry.getBoolean(false));
-            break;
-          case kBooleanArray:
-            table.put(key, entry.getBooleanArray(new boolean[0]));
-            break;
-          case kDouble:
-            table.put(key, entry.getDouble(0.0));
-            break;
-          case kDoubleArray:
-            table.put(key, entry.getDoubleArray(new double[0]));
-            break;
-          case kString:
-            table.put(key, entry.getString(""));
-            break;
-          case kStringArray:
-            table.put(key, entry.getStringArray(new String[0]));
-            break;
-          case kRaw:
-            table.put(key, entry.getRaw(new byte[0]));
-          default:
-            break;
-        }
-      }
-    }
+  //   for (int tableId = 0; tableId < tables.size(); tableId++) {
+  //     NetworkTableEntry[] entries = networkTables.getEntries(tables.get(tableId), 0);
+  //     for (int entryId = 0; entryId < entries.length; entryId++) {
+  //       NetworkTableEntry entry = entries[entryId];
+  //       String key = entry.getName().substring(1);
+  //       switch (entry.getType()) {
+  //         case kBoolean:
+  //           table.put(key, entry.getBoolean(false));
+  //           break;
+  //         case kBooleanArray:
+  //           table.put(key, entry.getBooleanArray(new boolean[0]));
+  //           break;
+  //         case kDouble:
+  //           table.put(key, entry.getDouble(0.0));
+  //           break;
+  //         case kDoubleArray:
+  //           table.put(key, entry.getDoubleArray(new double[0]));
+  //           break;
+  //         case kString:
+  //           table.put(key, entry.getString(""));
+  //           break;
+  //         case kStringArray:
+  //           table.put(key, entry.getStringArray(new String[0]));
+  //           break;
+  //         case kRaw:
+  //           table.put(key, entry.getRaw(new byte[0]));
+  //         default:
+  //           break;
+  //       }
+  //     }
+  //   }
   }
 
   public void fromLog(LogTable table) {
-    NetworkTable netTable = networkTables.getTable("/");
+    // NetworkTable netTable = networkTables.getTable("/");
 
-    for (Map.Entry<String, LogValue> mapEntry : table.getAll(true).entrySet()) {
-      NetworkTableEntry tableEntry = netTable.getEntry(mapEntry.getKey());
+    // for (Map.Entry<String, LogValue> mapEntry : table.getAll(true).entrySet()) {
+    //   NetworkTableEntry tableEntry = netTable.getEntry(mapEntry.getKey());
 
-      switch (mapEntry.getValue().type) {
-        case Boolean:
-          tableEntry.setBoolean(mapEntry.getValue().getBoolean());
-          break;
-        case BooleanArray:
-          tableEntry.setBooleanArray(mapEntry.getValue().getBooleanArray());
-          break;
-        case Integer:
-          tableEntry.setDouble(mapEntry.getValue().getInteger());
-          break;
-        case IntegerArray:
-          tableEntry.setDoubleArray(Arrays.stream(mapEntry.getValue().getIntegerArray()).asDoubleStream().toArray());
-          break;
-        case Double:
-          tableEntry.setDouble(mapEntry.getValue().getDouble());
-          break;
-        case DoubleArray:
-          tableEntry.setDoubleArray(mapEntry.getValue().getDoubleArray());
-          break;
-        case String:
-          tableEntry.setString(mapEntry.getValue().getString());
-          break;
-        case StringArray:
-          tableEntry.setStringArray(mapEntry.getValue().getStringArray());
-          break;
-        case ByteArray:
-          tableEntry.setRaw(mapEntry.getValue().getByteArray());
-          break;
-        default:
-          break;
-      }
-    }
+    //   switch (mapEntry.getValue().type) {
+    //     case Boolean:
+    //       tableEntry.setBoolean(mapEntry.getValue().getBoolean());
+    //       break;
+    //     case BooleanArray:
+    //       tableEntry.setBooleanArray(mapEntry.getValue().getBooleanArray());
+    //       break;
+    //     case Integer:
+    //       tableEntry.setDouble(mapEntry.getValue().getInteger());
+    //       break;
+    //     case IntegerArray:
+    //       tableEntry.setDoubleArray(Arrays.stream(mapEntry.getValue().getIntegerArray()).asDoubleStream().toArray());
+    //       break;
+    //     case Double:
+    //       tableEntry.setDouble(mapEntry.getValue().getDouble());
+    //       break;
+    //     case DoubleArray:
+    //       tableEntry.setDoubleArray(mapEntry.getValue().getDoubleArray());
+    //       break;
+    //     case String:
+    //       tableEntry.setString(mapEntry.getValue().getString());
+    //       break;
+    //     case StringArray:
+    //       tableEntry.setStringArray(mapEntry.getValue().getStringArray());
+    //       break;
+    //     case ByteArray:
+    //       tableEntry.setRaw(mapEntry.getValue().getByteArray());
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // }
   }
 }
