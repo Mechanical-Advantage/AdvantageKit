427b0b19e4685de866b25ef7aae90cf1db04fae2
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-11-07 00:06:10 -0500

    Rename "Advantage Scope" to "AdvantageScope"

--- junction/core/src/org/littletonrobotics/junction/LogFileUtil.java
+++ junction/core/src/org/littletonrobotics/junction/LogFileUtil.java
@@ -24,7 +24,7 @@ public class LogFileUtil {
    * Finds the path to a log file for replay, using the following priorities:
    * 
    * 1. The value of the "AKIT_LOG_PATH" environment variable, if set
-   * 2. The file currently open in Advantage Scope, if available
+   * 2. The file currently open in AdvantageScope, if available
    * 3. The result of the prompt displayed to the user
    */
   public static String findReplayLog() {
@@ -35,7 +35,7 @@ public class LogFileUtil {
       return envPath;
     }
 
-    // Read file from Advantage Scope
+    // Read file from AdvantageScope
     Path advantageScopeTempPath = Paths.get(System.getProperty("java.io.tmpdir"), advantageScopeFileName);
     String advantageScopeLogPath = null;
     try (Scanner fileScanner = new Scanner(advantageScopeTempPath)) {
@@ -43,14 +43,14 @@ public class LogFileUtil {
     } catch (IOException e) {
     }
     if (advantageScopeLogPath != null) {
-      System.out.println("Using log from Advantage Scope - \"" + advantageScopeLogPath + "\"");
+      System.out.println("Using log from AdvantageScope - \"" + advantageScopeLogPath + "\"");
       return advantageScopeLogPath;
     }
 
     // Prompt on stdin
     Scanner scanner = new Scanner(System.in);
     System.out.print("No log provided with the " + environmentVariable
-        + " environment variable or through Advantage Scope. Enter path to file: ");
+        + " environment variable or through AdvantageScope. Enter path to file: ");
     String filename = scanner.nextLine();
     scanner.close();
     if (filename.charAt(0) == '\'' || filename.charAt(0) == '"') {
