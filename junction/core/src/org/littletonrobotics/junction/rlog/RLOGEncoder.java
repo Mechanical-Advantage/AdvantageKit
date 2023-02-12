6d2a89e3ee009bd56089abee485005fd233f3845
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-12 00:59:27 -0500

    Add RLOG encoder and server

---
+++ junction/core/src/org/littletonrobotics/junction/rlog/RLOGEncoder.java
@@ -0,0 +1,250 @@
+package org.littletonrobotics.junction.rlog;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.LogTable.LogValue;
+
+/** Converts log tables to the RLOG format. */
+class RLOGEncoder {
+  public static final byte logRevision = (byte) 1;
+
+  private ByteBuffer nextOutput;
+  private boolean isFirstTable = true;
+  private LogTable lastTable = new LogTable(0);
+  private Map<String, Short> keyIDs = new HashMap<>();
+  private short nextKeyID = 0;
+
+  /** Reads the encoded output of the last encoded table. */
+  public ByteBuffer getOutput() {
+    return nextOutput;
+  }
+
+  /**
+   * Encodes a single tables and returns the encoded output. Equivalent to calling
+   * "encodeTable()" and then "getOutput()"
+   */
+  public ByteBuffer getOutput(LogTable table) {
+    encodeTable(table);
+    return nextOutput;
+  }
+
+  /**
+   * Returns data required to start a new receiver (full contents of last table +
+   * all key IDs).
+   */
+  public ByteBuffer getNewcomerData() {
+    List<ByteBuffer> buffers = new ArrayList<>();
+
+    // Encode log revision
+    buffers.add(ByteBuffer.allocate(1).put(logRevision));
+
+    // Encode timestamp
+    buffers.add(encodeTimestamp(lastTable.getTimestamp() / 1000000.0));
+
+    // Encode key IDs
+    for (Map.Entry<String, Short> keyID : keyIDs.entrySet()) {
+      buffers.add(encodeKey(keyID.getValue(), keyID.getKey()));
+    }
+
+    // Encode fields
+    for (Map.Entry<String, LogValue> field : lastTable.getAll(false).entrySet()) {
+      buffers.add(encodeValue(keyIDs.get(field.getKey()), field.getValue()));
+    }
+
+    // Combine buffers
+    int capacity = 0;
+    for (ByteBuffer buffer : buffers) {
+      capacity += buffer.capacity();
+    }
+    ByteBuffer output = ByteBuffer.allocate(capacity);
+    for (ByteBuffer buffer : buffers) {
+      output.put(buffer.array());
+    }
+    return output;
+  }
+
+  /** Encodes a single table and stores the result. */
+  public void encodeTable(LogTable table) {
+    List<ByteBuffer> buffers = new ArrayList<>();
+
+    Map<String, LogValue> newMap = table.getAll(false);
+    Map<String, LogValue> oldMap = lastTable.getAll(false);
+
+    // Encode log revision
+    if (isFirstTable) {
+      buffers.add(ByteBuffer.allocate(1).put(logRevision));
+      isFirstTable = false;
+    }
+
+    // Encode timestamp
+    buffers.add(encodeTimestamp(table.getTimestamp() / 1000000.0));
+
+    // Encode new/changed fields
+    for (Map.Entry<String, LogValue> field : newMap.entrySet()) {
+      // Check if field has changed
+      LogValue newValue = field.getValue();
+      if (newValue.equals(oldMap.get(field.getKey()))) {
+        continue;
+      }
+
+      // Write new data
+      if (!keyIDs.containsKey(field.getKey())) {
+        keyIDs.put(field.getKey(), nextKeyID);
+        buffers.add(encodeKey(nextKeyID, field.getKey()));
+        nextKeyID++;
+      }
+      buffers.add(encodeValue(keyIDs.get(field.getKey()), newValue));
+    }
+
+    // Encode removed fields (no longer supported)
+    //
+    // for (Map.Entry<String, LogValue> field : oldMap.entrySet()) {
+    // if (!newMap.containsKey(field.getKey())) {
+    // buffers.add(encodeValue(keyIDs.get(field.getKey()), null));
+    // }
+    // }
+
+    // Update last table
+    lastTable = table;
+
+    // Combine buffers
+    int capacity = 0;
+    for (ByteBuffer buffer : buffers) {
+      capacity += buffer.capacity();
+    }
+    nextOutput = ByteBuffer.allocate(capacity);
+    for (ByteBuffer buffer : buffers) {
+      nextOutput.put(buffer.array());
+    }
+  }
+
+  private static ByteBuffer encodeTimestamp(double timestamp) {
+    ByteBuffer buffer = ByteBuffer.allocate(1 + Double.BYTES);
+    buffer.put((byte) 0);
+    buffer.putDouble(timestamp);
+    return buffer;
+  }
+
+  private static ByteBuffer encodeKey(short keyID, String key) {
+    try {
+      byte[] keyBytes = key.getBytes("UTF-8");
+      ByteBuffer buffer = ByteBuffer.allocate(1 + Short.BYTES + Short.BYTES + keyBytes.length);
+      buffer.put((byte) 1);
+      buffer.putShort(keyID);
+      buffer.putShort((short) keyBytes.length);
+      buffer.put(keyBytes);
+      return buffer;
+    } catch (UnsupportedEncodingException e) {
+      return ByteBuffer.allocate(0);
+    }
+  }
+
+  private static ByteBuffer encodeValue(short keyID, LogValue value) {
+    try {
+      // Generate key and type buffer
+      ByteBuffer keyBuffer = ByteBuffer.allocate(1 + Short.BYTES + 1);
+      keyBuffer.put((byte) 2);
+      keyBuffer.putShort(keyID);
+
+      // Generate value buffer (and type for key)
+      ByteBuffer valueBuffer;
+      switch (value.type) {
+        case Raw:
+          keyBuffer.put((byte) 10);
+          byte[] byteArray = value.getRaw();
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + byteArray.length);
+          valueBuffer.putShort((short) byteArray.length);
+          valueBuffer.put(byteArray);
+          break;
+        case Boolean:
+          keyBuffer.put((byte) 1);
+          valueBuffer = ByteBuffer.allocate(1).put(value.getBoolean() ? (byte) 1 : (byte) 0);
+          break;
+        case Integer: // Save as Integer (int32)
+          keyBuffer.put((byte) 3);
+          valueBuffer = ByteBuffer.allocate(Integer.BYTES).putInt((int) value.getInteger());
+          break;
+        case Float: // Save as Double
+          keyBuffer.put((byte) 5);
+          valueBuffer = ByteBuffer.allocate(Double.BYTES).putDouble(value.getFloat());
+          break;
+        case Double:
+          keyBuffer.put((byte) 5);
+          valueBuffer = ByteBuffer.allocate(Double.BYTES).putDouble(value.getDouble());
+          break;
+        case String:
+          keyBuffer.put((byte) 7);
+          String stringValue = value.getString();
+          byte[] stringBytes = stringValue.getBytes("UTF-8");
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + stringBytes.length);
+          valueBuffer.putShort((short) stringBytes.length);
+          valueBuffer.put(stringBytes);
+          break;
+        case BooleanArray:
+          keyBuffer.put((byte) 2);
+          boolean[] booleanArray = value.getBooleanArray();
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + booleanArray.length);
+          valueBuffer.putShort((short) booleanArray.length);
+          for (boolean i : booleanArray) {
+            valueBuffer.put(i ? (byte) 1 : (byte) 0);
+          }
+          break;
+        case IntegerArray: // Save as IntegerArray (int32[])
+          keyBuffer.put((byte) 4);
+          long[] intArray = value.getIntegerArray();
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + (intArray.length * Integer.BYTES));
+          valueBuffer.putShort((short) intArray.length);
+          for (long i : intArray) {
+            valueBuffer.putInt((int) i);
+          }
+          break;
+        case FloatArray: // Save as DoubleArray
+          keyBuffer.put((byte) 6);
+          float[] floatArray = value.getFloatArray();
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + (floatArray.length * Double.BYTES));
+          valueBuffer.putShort((short) floatArray.length);
+          for (float i : floatArray) {
+            valueBuffer.putDouble(i);
+          }
+          break;
+        case DoubleArray:
+          keyBuffer.put((byte) 6);
+          double[] doubleArray = value.getDoubleArray();
+          valueBuffer = ByteBuffer.allocate(Short.BYTES + (doubleArray.length * Double.BYTES));
+          valueBuffer.putShort((short) doubleArray.length);
+          for (double i : doubleArray) {
+            valueBuffer.putDouble(i);
+          }
+          break;
+        case StringArray:
+          keyBuffer.put((byte) 8);
+          String[] stringArray = value.getStringArray();
+          int capacity = Short.BYTES;
+          for (String i : stringArray) {
+            capacity += Short.BYTES + i.getBytes("UTF-8").length;
+          }
+          valueBuffer = ByteBuffer.allocate(capacity);
+          valueBuffer.putShort((short) stringArray.length);
+          for (String i : stringArray) {
+            byte[] bytes = i.getBytes("UTF-8");
+            valueBuffer.putShort((short) bytes.length);
+            valueBuffer.put(bytes);
+          }
+          break;
+        default:
+          valueBuffer = ByteBuffer.allocate(0);
+      }
+
+      return ByteBuffer.allocate(keyBuffer.capacity() + valueBuffer.capacity()).put(keyBuffer.array())
+          .put(valueBuffer.array());
+    } catch (UnsupportedEncodingException e) {
+      return ByteBuffer.allocate(0);
+    }
+  }
+}
---
+++ junction/core/src/org/littletonrobotics/junction/rlog/RLOGServer.java
@@ -0,0 +1,144 @@
+package org.littletonrobotics.junction.rlog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.LogDataReceiver;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.Logger;
+
+/** Sends log data over a socket connection using the RLOG format. */
+public class RLOGServer implements LogDataReceiver {
+  private final int port;
+  private ServerThread thread;
+
+  public RLOGServer(int port) {
+    this.port = port;
+  }
+
+  public void start() {
+    thread = new ServerThread(port);
+    thread.start();
+    System.out.println("Log server started on port " + Integer.toString(port));
+  }
+
+  public void end() {
+    if (thread != null) {
+      thread.close();
+      thread = null;
+    }
+  }
+
+  public void putTable(LogTable table) {
+    if (thread != null) {
+      thread.periodic(table);
+    }
+  }
+
+  private class ServerThread extends Thread {
+    private static final double heartbeatTimeoutSecs = 3.0; // Close connection if hearbeat not received for this length
+
+    ServerSocket server;
+    RLOGEncoder encoder = new RLOGEncoder();
+
+    List<Socket> sockets = new ArrayList<>();
+    List<Double> lastHeartbeats = new ArrayList<>();
+
+    public ServerThread(int port) {
+      super("LogSocketServer");
+      this.setDaemon(true);
+      try {
+        server = new ServerSocket(port);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public void run() {
+      if (server == null) {
+        return;
+      }
+      while (true) {
+        try {
+          Socket socket = server.accept();
+          socket.getOutputStream().write(encodeData(encoder.getNewcomerData().array()));
+          sockets.add(socket);
+          lastHeartbeats.add(Logger.getInstance().getRealTimestamp() / 1000000.0);
+          System.out.println("Connected to log client - " + socket.getInetAddress().getHostAddress());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    public void periodic(LogTable table) {
+      if (server == null) {
+        return;
+      }
+
+      encoder.encodeTable(table);
+      byte[] data = encodeData(encoder.getOutput().array());
+      for (int i = 0; i < sockets.size(); i++) {
+        Socket socket = sockets.get(i);
+        if (socket.isClosed()) {
+          continue;
+        }
+
+        try {
+          // Read heartbeat
+          InputStream inputStream = socket.getInputStream();
+          if (inputStream.available() > 0) {
+            inputStream.skip(inputStream.available());
+            lastHeartbeats.set(i, Logger.getInstance().getRealTimestamp() / 1000000.0);
+          }
+
+          // Close connection if socket timed out
+          if (Logger.getInstance().getRealTimestamp() / 1000000.0 - lastHeartbeats.get(i) > heartbeatTimeoutSecs) {
+            socket.close();
+            printDisconnectMessage(socket, "timeout");
+          } else {
+
+            // Send new data
+            socket.getOutputStream().write(data);
+          }
+        } catch (IOException e) {
+          try {
+            socket.close();
+            printDisconnectMessage(socket, "IOException");
+          } catch (IOException a) {
+            a.printStackTrace();
+          }
+        }
+      }
+    }
+
+    private byte[] encodeData(byte[] data) {
+      byte[] lengthBytes = ByteBuffer.allocate(Integer.BYTES).putInt(data.length).array();
+      byte[] fullData = new byte[lengthBytes.length + data.length];
+      System.arraycopy(lengthBytes, 0, fullData, 0, lengthBytes.length);
+      System.arraycopy(data, 0, fullData, lengthBytes.length, data.length);
+      return fullData;
+    }
+
+    private void printDisconnectMessage(Socket socket, String reason) {
+      System.out.println("Disconnected from log client (" + reason + ") - " + socket.getInetAddress().getHostAddress());
+    }
+
+    public void close() {
+      if (server != null) {
+        try {
+          server.close();
+          server = null;
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      this.interrupt();
+    }
+  }
+}
