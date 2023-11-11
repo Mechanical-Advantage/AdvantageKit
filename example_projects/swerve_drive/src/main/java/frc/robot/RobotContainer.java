8c873c9a2417d6878976139bc6f93c463a2b0c18
Jonah <47046556+jwbonner@users.noreply.github.com>
2023-11-11 16:24:40 -0500

    Fix `AutoBuilder` import in swerve example project

--- example_projects/swerve_drive/src/main/java/frc/robot/RobotContainer.java
+++ example_projects/swerve_drive/src/main/java/frc/robot/RobotContainer.java
@@ -13,8 +13,8 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
