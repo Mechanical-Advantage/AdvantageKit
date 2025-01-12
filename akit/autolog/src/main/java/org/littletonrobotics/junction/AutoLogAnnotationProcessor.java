90346b7dd13e4c51c1a3c1483e1bbd307ef2b00c
Todd Anderson <tea2009@comcast.net>
2025-01-11 16:03:27 -0800

    import + tested

--- akit/autolog/src/main/java/org/littletonrobotics/junction/AutoLogAnnotationProcessor.java
+++ akit/autolog/src/main/java/org/littletonrobotics/junction/AutoLogAnnotationProcessor.java
@@ -20,6 +20,8 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
 import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import java.io.IOException;
 import java.util.HashMap;
