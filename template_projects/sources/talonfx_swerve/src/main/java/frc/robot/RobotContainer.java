6b287da30b7f28a599e501d69a9bfe623727eb92
Jonah <47046556+jwbonner@users.noreply.github.com>
2025-10-15 00:27:25 -0400

    Add TalonFXS/CANdi support to swerve template

--- template_projects/sources/talonfx_swerve/src/main/java/frc/robot/RobotContainer.java
+++ template_projects/sources/talonfx_swerve/src/main/java/frc/robot/RobotContainer.java
@@ -47,6 +47,7 @@ public class RobotContainer {
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
+        // ModuleIOTalonFX is intended for modules with TalonFX drive, TalonFX turn, and a CANcoder
         drive =
             new Drive(
                 new GyroIOPigeon2(),
@@ -54,6 +55,21 @@ public class RobotContainer {
                 new ModuleIOTalonFX(TunerConstants.FrontRight),
                 new ModuleIOTalonFX(TunerConstants.BackLeft),
                 new ModuleIOTalonFX(TunerConstants.BackRight));
+
+        // The ModuleIOTalonFXS implementation provides an example implementation for
+        // TalonFXS controller connected to a CANdi with a PWM encoder. The implementations
+        // of ModuleIOTalonFX, ModuleIOTalonFXS, and ModuleIOSpark (from the Spark swerve
+        // template) can be freely intermixed to support alternative hardware arrangements.
+        // Please see the AdvantageKit template documentation for more information:
+        // https://docs.advantagekit.org/getting-started/template-projects/talonfx-swerve-template#custom-module-implementations
+        //
+        // drive =
+        //     new Drive(
+        //         new GyroIOPigeon2(),
+        //         new ModuleIOTalonFXS(TunerConstants.FrontLeft),
+        //         new ModuleIOTalonFXS(TunerConstants.FrontRight),
+        //         new ModuleIOTalonFXS(TunerConstants.BackLeft),
+        //         new ModuleIOTalonFXS(TunerConstants.BackRight));
         break;
 
       case SIM:
