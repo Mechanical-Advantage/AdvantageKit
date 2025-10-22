8435e3647f8124384c4d727fa61fef27e4b93f1f
Jade Turner <spacey-sooty@proton.me>
2025-10-22 21:57:17 +0800

    Fix misnamed function parameter documentation in VisionIOPhotonVision
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- template_projects/sources/vision/src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
+++ template_projects/sources/vision/src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
@@ -27,7 +27,7 @@ public class VisionIOPhotonVision implements VisionIO {
    * Creates a new VisionIOPhotonVision.
    *
    * @param name The configured name of the camera.
-   * @param rotationSupplier The 3D position of the camera relative to the robot.
+   * @param robotToCamera The 3D position of the camera relative to the robot.
    */
   public VisionIOPhotonVision(String name, Transform3d robotToCamera) {
     camera = new PhotonCamera(name);
