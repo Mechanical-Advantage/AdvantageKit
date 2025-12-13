42b2f8590cfc033fdc02605cf137ce43e1d3be39
Jonah Bonner <47046556+jwbonner@users.noreply.github.com>
2025-12-13 13:51:17 +0300

    Update to WPILib 2026 (#233)

--- template_projects/sources/vision/src/main/java/frc/robot/Robot.java
+++ template_projects/sources/vision/src/main/java/frc/robot/Robot.java
@@ -108,7 +108,7 @@ public class Robot extends LoggedRobot {
 
     // schedule the autonomous command (example)
     if (autonomousCommand != null) {
-      autonomousCommand.schedule();
+      CommandScheduler.getInstance().schedule(autonomousCommand);
     }
   }
 
