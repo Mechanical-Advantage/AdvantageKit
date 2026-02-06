fbf438852d3e4aa7ad6e749dff876e46aefa3f70
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-02-06 08:38:41 -0600

    feat: Add LoggableInputs and more LogTable functions

---
+++ akit/src/main/native/include/akit/inputs/LoggableInputs.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+#pragma once
+
+namespace akit {
+
+class LogTable;
+
+namespace inputs {
+
+class LoggableInputs {
+public:
+	virtual void toLog(LogTable) = 0;
+	virtual void fromLog(LogTable) = 0;
+};
+
+}
+
+}
