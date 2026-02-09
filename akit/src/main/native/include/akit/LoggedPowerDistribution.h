f5888f0e46c27e712b12eec2672b9aa072e5e265
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-02-08 19:03:05 -0600

    feat: Implement LoggedDriverStation

--- akit/src/main/native/include/akit/LoggedPowerDistribution.h
+++ akit/src/main/native/include/akit/LoggedPowerDistribution.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
 #pragma once
 #include "akit/LogTable.h"
 #include "akit/conduit/ConduitApi.h"
@@ -5,45 +12,53 @@
 namespace akit {
 
 class LoggedPowerDistribution {
-    public:
-    static LoggedPowerDistribution& getInstance() {
-        if (!instance)
-            instance = std::make_unique<LoggedPowerDistribution>();
-        return instance;
-    }
+public:
+	static LoggedPowerDistribution& getInstance() {
+		if (!instance)
+			instance = std::make_unique<LoggedPowerDistribution>();
+		return instance;
+	}
 
-    static LoggedPowerDistribution& getInstance(int moduleID, HAL_PowerDistributionType moduleType) {
-        if (!instance) instance = std::make_unique<LoggedPowerDistribution>(moduleID, moduleType);
-        return instance;
-    }
+	static LoggedPowerDistribution& getInstance(int moduleID,
+			HAL_PowerDistributionType moduleType) {
+		if (!instance)
+			instance = std::make_unique < LoggedPowerDistribution
+					> (moduleID, moduleType);
+		return instance;
+	}
 
-    void saveToLog(LogTable table) {
-        conduit::ConduitApi& inst = conduit::ConduitApi::getInstance();
-        table.put("Temperature", inst.getPDPTemperature());
-        table.put("Voltage", inst.getPDPVoltage());
-        std::array<double, 24> currents;
-        for (int i = 0; i < currents.size(); i++)
-            currents[i] = inst.getPDPChannelCurrent(i);
-        table.put("ChannelCurrent", currents);
-        table.put("TotalCurrent", inst.getPDPTotalCurrent());
-        table.put("TotalPower", inst.getPDPTotalPower());
-        table.put("TotalEnergy", inst.getPDPTotalEnergy());
+	void saveToLog(LogTable table) {
+		conduit::ConduitApi &inst = conduit::ConduitApi::getInstance();
+		table.put("Temperature", inst.getPDPTemperature());
+		table.put("Voltage", inst.getPDPVoltage());
+		std::array<double, 24> currents;
+		for (int i = 0; i < currents.size(); i++)
+			currents[i] = inst.getPDPChannelCurrent(i);
+		table.put("ChannelCurrent", currents);
+		table.put("TotalCurrent", inst.getPDPTotalCurrent());
+		table.put("TotalPower", inst.getPDPTotalPower());
+		table.put("TotalEnergy", inst.getPDPTotalEnergy());
 
-        table.put("ChannelCount", inst.getPDPChannelCount());
-        table.put("Faults", inst.getPDPFaults());
-        table.put("StickyFaults", inst.getPDPStickyFaults());
-    }
+		table.put("ChannelCount", inst.getPDPChannelCount());
+		table.put("Faults", inst.getPDPFaults());
+		table.put("StickyFaults", inst.getPDPStickyFaults());
+	}
 
-    private:
-    LoggedPowerDistribution() : LoggedPowerDistribution{HAL_DEFAULT_POWER_DISTRIBUTION_MODULE, HAL_PowerDistributionType_kAutomatic} {}
+private:
+	LoggedPowerDistribution() : LoggedPowerDistribution {
+			HAL_DEFAULT_POWER_DISTRIBUTION_MODULE,
+			HAL_PowerDistributionType_kAutomatic } {
+	}
 
-    LoggedPowerDistribution(int moduleID, HAL_PowerDistributionType moduleType) : moduleID{moduleID}, moduleType{moduleType} {
-        conduit::ConduitApi::getInstance().configurePowerDistribution(moduleID, moduleType);
-    }
+	LoggedPowerDistribution(int moduleID, HAL_PowerDistributionType moduleType) : moduleID {
+			moduleID }, moduleType { moduleType } {
+		conduit::ConduitApi::getInstance().configurePowerDistribution(moduleID,
+				moduleType);
+	}
 
-    static std::unique_ptr<LoggedPowerDistribution> instance;
-    int moduleID;
-    HAL_PowerDistributionType moduleType;
+	static std::unique_ptr<LoggedPowerDistribution> instance;
+	int moduleID;
+	HAL_PowerDistributionType moduleType;
 }
 
 }
