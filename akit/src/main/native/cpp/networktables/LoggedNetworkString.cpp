4601c40810a384e8b201df60bf7277adac15ba04
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-02-08 19:44:53 -0600

    feat: Clean up LoggedNetworkString

---
+++ akit/src/main/native/cpp/networktables/LoggedNetworkString.cpp
@@ -0,0 +1,33 @@
+#include "akit/networktables/LoggedNetworkString.h"
+
+using namespace akit::nt;
+
+LoggedNetworkString::LoggedNetworkString(std::string key) : key{key}, entry{::nt::NetworkTableInstance::GetDefault().GetStringTopic(key).GetEntry("")} {}
+
+LoggedNetworkString::LoggedNetworkString(std::string key, std::string defaultValue) : LoggedNetworkString{key} {
+    setDefault(defaultValue);
+    value = defaultValue;
+}
+
+void LoggedNetworkString::setDefault(std::string defaultValue) {
+    this->defaultValue = defaultValue;
+    entry.Set(entry.Get(defaultValue));
+}
+
+void LoggedNetworkString::set(std::string value) {
+    entry.Set(value);
+}
+
+std::string LoggedNetworkString::get() {
+    return value;
+}
+
+void LoggedNetworkString::toLog(LogTable&& table) {
+    table.put(removeSlash(key), value);
+}
+
+void LoggedNetworkString::fromLog(LogTable&& table) {
+    value = table.get(removeSlash(key), defaultValue);
+}
+
+void LoggedNetworkString::periodic() {}
