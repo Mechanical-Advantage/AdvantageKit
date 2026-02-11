befc50cd510e369e225adfde70d50e6a4ddb1269
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-02-10 18:24:33 -0600

    feat: Created header files

---
+++ akit/src/main/native/include/akit/mechanism/LoggedMechanismLigament2d.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+#pragma once
+#include <units/mass.h>
+#include <networktables/StringTopic.h>
+#include <networktables/DoubleTopic.h>
+#include <frc/util/Color8Bit.h>
+#include "akit/mechanism/LoggedMechanismObject2d.h"
+
+namespace akit {
+
+namespace mech {
+
+class LoggedMechanismLigament2d: public LoggedMechanismObject2d {
+public:
+	LoggedMechanismLigament2d(std::string name, units::meter_t length,
+			units::degree_t angle, double lineWidth, frc::Color8Bit color);
+
+	LoggedMechanismLigament2d(std::string name, units::meter_t length,
+			units::degree_t angle) : LoggedMechanismLigament2d { name, length,
+			angle, 10, frc::Color8Bit { 235, 137, 52 } } {
+	}
+
+	void setAngle(units::degree_t angle);
+
+	void setAngle(frc::Rotation2d angle);
+
+	units::degree_t getAngle() override;
+
+	void setLength(units::meter_t);
+
+	units::meter_t getLength();
+
+	void setColor(frc::Color8Bit color);
+
+	frc::Color8Bit getColor();
+
+	void setLineWeight(double weight);
+
+	double getLineWeight();
+
+protected:
+	void updateEntries(std::shared_ptr<nt::NetworkTable> table) override;
+
+private:
+	void logTable(LogTable &&table);
+
+	units::meter_t getObject2dRange() override {
+		return getLength();
+	}
+
+	nt::StringPublisher typePub;
+	units::degree_t angle;
+	nt::DoubleEntry angleEntry;
+	std::string color;
+	nt::StringEntry colorEntry;
+	units::meter_t length;
+	nt::DoubleEntry lengthEntry;
+	units::pound_t weight;
+	nt::DoubleEntry weightEntry;
+};
+
+}
+
+}
---
+++ akit/src/main/native/include/akit/mechanism/LoggedMechanismRoot2d.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+#pragma once
+#include <networktables/DoubleTopic.h>
+#include "akit/mechanism/LoggedMechanismObject2d.h"
+
+namespace akit {
+
+namespace mech {
+
+class LoggedMechanismRoot2d {
+public:
+	LoggedMechanismObject2d append(LoggedMechanismObject2d object);
+
+	void setPosition(units::meter_t x, units::meter_t y);
+
+	std::string getName() {
+		return name;
+	}
+
+	std::vector<frc::Pose3d> generate3dMechanism();
+
+private:
+	LoggedMechanismRoot2d(std::string name, units::meter_t x, units::meter_t y) : name {
+			name }, x { x }, y { y } {
+	}
+
+	void update(std::shared_ptr<nt::NetworkTable> table);
+
+	void flush();
+
+	void logOutput(LogTable &&table);
+
+	std::string name;
+	std::shared_ptr<nt::NetworkTable> table;
+	std::unordered_map<std::string, LoggedMechanismObject2d> objects;
+	units::meter_t x;
+	nt::DoublePublisher xPub;
+	units::meter_t y;
+	nt::DoublePublisher yPub;
+};
+
+}
+
+}
