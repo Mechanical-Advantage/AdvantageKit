f26555d6bd25563bfbe60ef258b0aaafcfa508ad
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-02-10 18:24:33 -0600

    feat: Created header files

---
+++ akit/src/main/native/include/akit/mechanism/LoggedMechanismObject2d.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+#pragma once
+#include <networktables/NetworkTable.h>
+#include <frc/geometry/Pose3d.h>
+#include "akit/LogTable.h"
+
+namespace akit {
+
+namespace mech {
+
+class LoggedMechanismObject2d {
+public:
+	virtual units::meter_t getObject2dRange() = 0;
+
+	virtual units::degree_t getAngle() = 0;
+
+	std::vector<frc::Pose3d> generate3dMechanism(frc::Pose3d seed);
+
+	std::string getName() {
+		return name;
+	}
+
+protected:
+	LoggedMechanismObject2d(std::string name) : name { name } {
+	}
+
+	LoggedMechanismObject2d append(LoggedMechanismObject2d object);
+
+	virtual void updateEntries(std::shared_ptr<nt::NetworkTable> table) = 0;
+
+private:
+	void logOutput(LogTable &&table);
+
+	void update(std::shared_ptr<nt::NetworkTable> table);
+
+	std::string name;
+	std::shared_ptr<nt::NetworkTable> table;
+	std::unordered_map<std::string, LoggedMechanismObject2d> objects;
+};
+
+}
+
+}
