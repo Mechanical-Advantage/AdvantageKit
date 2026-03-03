64198874a9bb2e068aba01986a516e53ecd5ba08
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-03-03 17:26:48 -0600

    feat: Working on working ConsoleSource

--- akit/src/main/native/cpp/ConsoleSource.cpp
+++ akit/src/main/native/cpp/ConsoleSource.cpp
@@ -8,38 +8,98 @@
 #include <fstream>
 #include <frc/Errors.h>
 #include <vector>
+#include <fcntl.h>
+#ifdef _WIN32
+#include <io.h>
+#include <windows.h>
+#endif
 #include "akit/ConsoleSource.h"
 
 using namespace akit;
 
-SimulatorConsoleSource::SimulatorConsoleSource() : originalCout {
-		std::cout.rdbuf(&splitCout) }, originalCerr { std::cerr.rdbuf(
-		&splitCerr) } {
+SimulatorConsoleSource::SimulatorConsoleSource() {
+#ifdef _WIN32
+	_pipe(stdoutPipe, 4096, _O_TEXT);
+	_pipe(stderrPipe, 4096, _O_TEXT);
+	originalCout = _dup(_fileno(stdout));
+	originalCerr = _dup(_fileno(stderr));
+	_dup2(stdoutPipe[1], _fileno(stdout));
+	_dup2(stderrPipe[1], _fileno(stderr));
+#else
+	pipe(stdoutPipe);
+	pipe(stderrPipe);
+	originalCout = dup(STDOUT_FILENO);
+	originalCerr = dup(STDERR_FILENO);
+
+	dup2(stdoutPipe[1], STDOUT_FILENO);
+	dup2(stderrPipe[1], STDERR_FILENO);
+
+	close(stdoutPipe[1]);
+	close(stderrPipe[1]);
+#endif
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
 }
 
 SimulatorConsoleSource::~SimulatorConsoleSource() {
-	std::cout.rdbuf(originalCout);
-	std::cerr.rdbuf(originalCerr);
+	running = false;
+	thread.join();
+
+#ifdef _WIN32
+	_dup2(originalCout, _fileno(stdout));
+	_dup2(originalCerr, _fileno(stderr));
+	_close(originalCout);
+	_close(originalCerr);
+#else
+	dup2(originalCout, STDOUT_FILENO);
+	dup2(originalCerr, STDERR_FILENO);
+
+	close(originalCout);
+	close(originalCerr);
+#endif
+}
+
+void SimulatorConsoleSource::Run() {
+	char buffer[1024];
+	while (running) {
+#ifdef _WIN32
+		DWORD bytesAvailable = 0;
+		HANDLE hStdOut = reinterpret_cast<HANDLE>(_get_osfhandle(stdoutPipe[0]));
+		if (PeekNamedPipe(hStdOut, NULL, 0, NULL, &bytesAvailable, NULL) && bytesAvailable > 0) {
+			int count = _read(stdoutPipe[0], buffer, sizeof(buffer));
+			if (count > 0) _write(originalCout, buffer, count);
+		}
+		HANDLE hStdErr = reinterpret_cast<HANDLE>(_get_osfhandle(stderrPipe[0]));
+		if (PeekNamedPipe(hStdErr, NULL, 0, NULL, &bytesAvailable, NULL) && bytesAvailable > 0) {
+			int count = _read(stderrPipe[0], buffer, sizeof(buffer));
+			if (count > 0) _write(originalCerr, buffer, count);
+		}
+#else
+#endif
+
+		// int count = read(stdoutPipe[0], buffer, sizeof(buffer));
+		// if (count > 0)
+		// 	write(originalCout, buffer, count);
+
+		// count = read(stderrPipe[0], buffer, sizeof(buffer));
+		// if (count > 0)
+		// 	write(originalCerr, buffer, count);
+		std::this_thread::sleep_for(std::chrono::milliseconds{5});
+	}
 }
 
 std::string SimulatorConsoleSource::GetNewData() {
-	std::string fullOut = capturedCout.str();
-	std::string newOut = fullOut.substr(coutPos);
-	coutPos = fullOut.size();
+	return "";
+	// std::string fullOut = capturedCout.str();
+	// std::string newOut = fullOut.substr(coutPos);
+	// coutPos = fullOut.size();
 
-	std::string fullErr = capturedCerr.str();
-	std::string newErr = fullErr.substr(coutPos);
-	cerrPos = fullErr.size();
+	// std::string fullErr = capturedCerr.str();
+	// std::string newErr = fullErr.substr(cerrPos);
+	// cerrPos = fullErr.size();
 
-	return newOut + newErr;
-}
-
-int SimulatorConsoleSource::SplitBuffer::overflow(int c) {
-	if (c == EOF)
-		return !EOF;
-	original->sputc(c);
-	capture.put(static_cast<char>(c));
-	return c;
+	// return newOut + newErr;
 }
 
 RoboRIOConsoleSource::~RoboRIOConsoleSource() {
