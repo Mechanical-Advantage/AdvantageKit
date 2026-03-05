59002be2b0b83e9fcdd749ca250f0b311118778b
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-03-05 08:53:11 -0600

    feat: Get WPILOGWriter implementation

--- akit/src/main/native/include/akit/wpilog/WPILOGWriter.h
+++ akit/src/main/native/include/akit/wpilog/WPILOGWriter.h
@@ -17,27 +17,12 @@ namespace wpilog {
 class WPILOGWriter: public LogDataReceiver {
 public:
 	enum class AdvantageScopeOpenBehavior {
-		ALWAYS, AUTO, NEVER
+		ALWAYS, AUTO, NEVER,
 	};
 
-	WPILOGWriter(std::string path, AdvantageScopeOpenBehavior openBehavior);
-
-	WPILOGWriter(std::string path) : WPILOGWriter { path,
-			AdvantageScopeOpenBehavior::AUTO } {
-	}
-
-	WPILOGWriter(AdvantageScopeOpenBehavior openBehavior) : WPILOGWriter {
-			std::string {
-					frc::RobotBase::IsSimulation() ?
-							DEFAULT_PATH_SIM : DEFAULT_PATH_RIO }, openBehavior } {
-	}
-
-	WPILOGWriter() : WPILOGWriter {
-			std::string {
-					frc::RobotBase::IsSimulation() ?
-							DEFAULT_PATH_SIM : DEFAULT_PATH_RIO },
-			AdvantageScopeOpenBehavior::AUTO } {
-	}
+	WPILOGWriter(std::string_view path = DEFAULT_PATH,
+			AdvantageScopeOpenBehavior openBehavior =
+					AdvantageScopeOpenBehavior::AUTO);
 
 	void Start() override;
 
@@ -46,29 +31,30 @@ public:
 	void PutTable(LogTable &table) override;
 
 private:
-	static constexpr double TIMESTAMP_UPDATE_DELAY = 5;
-	static constexpr std::string_view DEFAULT_PATH_RIO = "/U/logs";
-	static constexpr std::string_view DEFAULT_PATH_SIM = "logs";
-	static constexpr std::string_view ADVANTAGESCOPE_FILE_NAME =
+	static constexpr units::second_t TIMESTAMP_UPDATE_DELAY = 5_s;
+	static constexpr std::string_view DEFAULT_PATH =
+			frc::RobotBase::IsSimulation() ? "logs" : "/U/logs";
+	static constexpr std::string_view TIME_FORMATTER = "{:%y-%m-%d_%H-%M-%S}";
+	static constexpr std::string_view ADVANTAGESCOPE_FILENAME =
 			"ascope-log-path.txt";
 
-	std::string folder;
-	std::string filename;
-	std::string randomIdentifier;
-	std::optional<double> dsAttachedTime;
+	static std::string GetUnitMetadata(std::string_view unit);
 
-	bool autoRename;
-	std::optional<std::chrono::system_clock::time_point> logDate;
-	std::string logMatchText;
-
-	std::unique_ptr<wpi::log::DataLogWriter> log;
 	bool isOpen = false;
-	AdvantageScopeOpenBehavior openBehavior;
-	std::optional<LogTable> lastTable;
+	bool autoRename;
 	int timestampID;
+	LogTable lastTable { 0_s };
+	AdvantageScopeOpenBehavior openBehavior;
+	std::string logMatchText;
+	std::string randomIdentifier;
+	std::filesystem::path folder;
+	std::filesystem::path filename;
+	std::unique_ptr<wpi::log::DataLogWriter> log;
 	std::unordered_map<std::string, int> entryIDs;
 	std::unordered_map<std::string, LogTable::LoggableType> entryTypes;
 	std::unordered_map<std::string, std::string> entryUnits;
+	std::optional<std::chrono::local_time<std::chrono::system_clock::duration>> logDate;
+	std::optional<units::millisecond_t> dsAttachedTime;
 };
 
 }
