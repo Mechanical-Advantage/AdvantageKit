67085ee2635c06314fe85e5ec830a94f834871f5
Jonah <47046556+jwbonner@users.noreply.github.com>
2026-08-30 11:49:18 -0400

    Restore AlertLogger

--- akit/src/main/java/org/littletonrobotics/junction/AlertLogger.java
+++ akit/src/main/java/org/littletonrobotics/junction/AlertLogger.java
@@ -7,9 +7,58 @@
 
 package org.littletonrobotics.junction;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.wpilib.util.AlertDataJNI;
+
 class AlertLogger {
+  private static final Set<String> knownGroups = new HashSet<>();
+
   /** Log the current state of all alerts as outputs. */
   public static void periodic() {
-    // TODO: Alerts are not functional as of WPILib 2027.0.0-alpha-6
+    AlertDataJNI.AlertInfo[] allAlerts = AlertDataJNI.getAlerts();
+
+    Map<String, List<AlertDataJNI.AlertInfo>> activeByGroup = new HashMap<>();
+
+    for (AlertDataJNI.AlertInfo info : allAlerts) {
+      knownGroups.add(info.group);
+      if (info.activeStartTime != 0) {
+        activeByGroup.computeIfAbsent(info.group, g -> new ArrayList<>()).add(info);
+      }
+    }
+
+    for (String group : knownGroups) {
+      List<AlertDataJNI.AlertInfo> groupAlerts =
+          activeByGroup.getOrDefault(group, Collections.emptyList());
+
+      // Sort by most recent first, then text ascending
+      groupAlerts.sort(
+          Comparator.comparingLong((AlertDataJNI.AlertInfo a) -> a.activeStartTime)
+              .reversed()
+              .thenComparing(a -> a.text));
+
+      List<String> errors = new ArrayList<>();
+      List<String> warnings = new ArrayList<>();
+      List<String> infos = new ArrayList<>();
+
+      for (AlertDataJNI.AlertInfo alert : groupAlerts) {
+        switch (alert.level) {
+          case AlertDataJNI.LEVEL_HIGH -> errors.add(alert.text);
+          case AlertDataJNI.LEVEL_MEDIUM -> warnings.add(alert.text);
+          case AlertDataJNI.LEVEL_LOW -> infos.add(alert.text);
+        }
+      }
+
+      Logger.recordOutput(group + "/.type", "Alerts");
+      Logger.recordOutput(group + "/errors", errors.toArray(new String[0]));
+      Logger.recordOutput(group + "/warnings", warnings.toArray(new String[0]));
+      Logger.recordOutput(group + "/infos", infos.toArray(new String[0]));
+    }
   }
 }
